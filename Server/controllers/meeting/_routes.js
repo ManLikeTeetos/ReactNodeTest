8ba40dc9283c0b0d30b3b97071b2ecaa02db2cbf
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2025-06-13 01:43:20 +0000

    Api for add and view meeting on server side

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const meeting = require('./meeting');
+const auth    = require('../../middlewares/auth');
 
 const router = express.Router();
+
+router.post('/add', auth, meeting.add);
+router.get('/view/:id', auth, meeting.view)
+
 
 module.exports = router
--- Server/controllers/meeting/meeting.js
+++ Server/controllers/meeting/meeting.js
@@ -2,6 +2,18 @@ const MeetingHistory = require('../../model/schema/meeting')
 const mongoose = require('mongoose');
 
 const add = async (req, res) => {
+    try {
+        if(!req.body.agenda){
+            res.status(400).json({message: 'Agenda is required'});
+        }
+
+        const result = new MeetingHistory(req.body);
+        await result.save();
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Failed to create :', err);
+        res.status(400).json({ err, error: 'Failed to create' });
+    }
    
 }
 
@@ -10,7 +22,22 @@ const index = async (req, res) => {
 }
 
 const view = async (req, res) => {
-    
+    try {
+        const meeting = await MeetingHistory.findById(req.params.id)
+            .populate('attendes')
+            .populate('attendesLead')
+            .populate('createBy')
+            .exec();
+
+        if (!meeting || meeting.deleted) {
+            return res.status(404).json({ message: "Meeting not found" });
+        }
+
+        res.status(200).json(meeting);
+    } catch (error) {
+        console.error("View Meeting Error:", error);
+        res.status(500).json({ error });
+    }
 }
 
 const deleteData = async (req, res) => {
